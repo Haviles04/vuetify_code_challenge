5c936376af94bc1f04ce2693beca92dd52543940
Henry <haviles04@gmail.com>
2025-03-06 18:27:28 -0500

    fix: minor fixes

--- client/src/components/BlogSnippet.vue
+++ client/src/components/BlogSnippet.vue
@@ -6,18 +6,20 @@
     <v-card-title>
       {{ blog.title }}
     </v-card-title>
+    <v-card-subtitle>
+      By: {{ blog.author }}
+    </v-card-subtitle>
     <v-card-text>
-      {{ blog.text }}
+      {{ blog.text.slice(0, 250) }}...
     </v-card-text>
 
     <v-card-actions>
-      <v-btn>
-        <router-link
-          :to="`/blog/${blog.id}`"
-        >
-          View More
-        </router-link>
-      </v-btn>
+      <router-link
+        :to="`/blog/${blog.id}`"
+        class="v-btn v-btn--flat v-btn--text"
+      >
+        View More
+      </router-link>
     </v-card-actions>
   </v-card>
 </template>
